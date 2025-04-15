8f05ba82c9d57e95e585ad04231269dbcfa96f36
Alex Uribe <repzyyog@gmail.com>
2025-04-15 16:24:48 +0200

    Create Navigation component with styles

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import "./Navigation.css";
+
+const Navigation: React.FC = () => {
+  return (
+    <nav className="navigation">
+      <a className="navigation__link" href="/">
+        Pokemons
+      </a>
+    </nav>
+  );
+};
+
+export default Navigation;
