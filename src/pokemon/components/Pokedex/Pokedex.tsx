ecbb7d56b3103f4268fdb18a8da9516517848508
Alex Uribe <repzyyog@gmail.com>
2025-04-15 18:58:48 +0200

    Create the Pokedex component

---
+++ src/pokemon/components/Pokedex/Pokedex.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Pokemon } from "../../../types";
+
+interface PokedexProps {
+  pokemons: Pokemon[];
+}
+
+const Pokedex: React.FC<PokedexProps> = ({ pokemons }) => {
+  return (
+    <ul className="pokemons">
+      {pokemons.map((pokemon) => (
+        <li key={pokemon.id}></li>
+      ))}
+    </ul>
+  );
+};
+
+export default Pokedex;
