ca334dbd55e21363c05963117eb348d23a14b598
Alex Uribe <repzyyog@gmail.com>
2025-04-17 22:31:02 +0200

    Add createPokemon to PokemonContextProvider

--- src/pokemon/context/types.ts
+++ src/pokemon/context/types.ts
@@ -1,8 +1,9 @@
-import { Pokemon } from "../types";
+import { Pokemon, PokemonCommonData } from "../types";
 
 interface PokemonContextStructure {
   pokemons: Pokemon[];
   loadPokemons: () => Promise<void>;
+  createPokemon: (pokemonCommonData: PokemonCommonData) => Promise<Pokemon>;
 }
 
 export default PokemonContextStructure;
