8c3a5a1f1c202e526dacb547510a183f379b4f7c
Alex Uribe <repzyyog@gmail.com>
2025-04-17 22:24:29 +0200

    Create addPokemon and getPokemonPokedexPosition methods to PokemonClient

--- src/pokemon/client/types.ts
+++ src/pokemon/client/types.ts
@@ -1,7 +1,9 @@
-import { Pokemon } from "../types";
+import { Pokemon, PokemonCommonData } from "../types";
 
 export interface PokemonClientStructure {
   getPokemons: () => Promise<Pokemon[]>;
+  getPokemonPokedexPosition: (pokemonName: string) => Promise<string>;
+  addPokemon: (pokemonCommonData: PokemonCommonData) => Promise<Pokemon>;
 }
 export interface pokemonsTypes {
   types: { type: { name: string } }[];
