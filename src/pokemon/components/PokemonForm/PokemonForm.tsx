cebfb7e7b00f96946d748645256a46a63c3b6a7f
Alex Uribe <repzyyog@gmail.com>
2025-04-17 18:10:49 +0200

    Create PokemonForm component with styles

--- src/pokemon/components/PokemonForm/PokemonForm.tsx
+++ src/pokemon/components/PokemonForm/PokemonForm.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./PokemonForm.css";
 
 const PokemonForm: React.FC = () => {
-  return <form action=""></form>;
+  return (
+    <form className="pokemon-form" action="">
+      <div className="pokemon-form__group">
+        <label htmlFor="name" className="pokemon-form__text">
+          Name:
+        </label>
+        <input
+          type="text"
+          className="pokemon-form__control"
+          id="name"
+          required
+        />
+      </div>
+      <button className="pokemon-form__button" type="submit">
+        Add to pokedex
+      </button>
+    </form>
+  );
 };
 
 export default PokemonForm;
--- src/pokemon/pages/AddPokemonPage/AddPokemonPage.tsx
+++ src/pokemon/pages/AddPokemonPage/AddPokemonPage.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import PokemonForm from "../../components/PokemonForm/PokemonForm";
+import "./AddPokemonPage.css";
 
 const AddPokemonPage: React.FC = () => {
   return (
-    <main>
-      <h2>Add a new pokemon</h2>
-      <PokemonForm />
+    <main className="add-pokemon__border">
+      <div className="add-pokemon">
+        <div className="add-pokemon__form">
+          <h2 className="add-pokemon__title">Add a new pokemon</h2>
+          <PokemonForm />
+        </div>
+        <img src="/random-pokemons.gif" alt="" className="random-pokemon" />
+      </div>
     </main>
   );
 };
