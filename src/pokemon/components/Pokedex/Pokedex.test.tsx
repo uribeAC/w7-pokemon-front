ab378854c1c0fe2535f102c69df92ce5a8a3da4b
Alex Uribe <repzyyog@gmail.com>
2025-04-18 13:07:51 +0200

    Add delete functionality to button

--- src/pokemon/components/Pokedex/Pokedex.test.tsx
+++ src/pokemon/components/Pokedex/Pokedex.test.tsx
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Pokedex from "./Pokedex";
 import { eevees, flareon, jolteon, vaporeon } from "../../fixtures";
+import PokemonContextProvider from "../../context/PokemonContextProvider";
 
 describe("Given the Pokedex component", () => {
   describe("When it receives 'Vaporeon', 'Jolteon' and 'Flareon'", () => {
     test("Then it should show the names of 'Vaporeon', 'Jolteon' and 'Flareon' inside a heading", () => {
-      render(<Pokedex pokemons={eevees} />);
+      render(<Pokedex pokemons={eevees} />, {
+        wrapper: PokemonContextProvider,
+      });
 
       const vaporeonName = screen.getByRole("heading", { name: vaporeon.name });
       const jolteonName = screen.getByRole("heading", { name: jolteon.name });
